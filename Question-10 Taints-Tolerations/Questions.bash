d9d7b3db95671294326813995f3c1ab5514302d4
vj2201 <93169265+vj2201@users.noreply.github.com>
2025-11-29 11:48:18 +1300

    Update Questions.bash

--- Question-10 Taints-Tolerations/Questions.bash
+++ Question-10 Taints-Tolerations/Questions.bash
@@ -4,4 +4,4 @@
 # 1. Add a taint to node01 so tht no normal pods can be scheduled in this node. key=PERMISSION, value=granted, Type=NoSchedule
 # 2. Schedule a Pod on node01 adding the correct toleration to the spec so it can be deployed
 
-# Video Link
+# Video Link - https://youtu.be/oy6Mdqt1-jk
