88ba00873ec6da95e958e099fcf29492fc0e2567
Mohannad Banat <75941340+mohnad-0b@users.noreply.github.com>
2026-04-25 04:58:31 +0300

    Fix Helm repo alias in ArgoCD solution
    Fixed an issue in SolutionNotes.bash where the Helm template command
    was using an incorrect repository alias.
    
    The repo was added as "argocd":
      helm repo add argocd https://argoproj.github.io/argo-helm
    
    But the template command incorrectly used:
      argo/argo-cd
    
    This has been corrected to:
      argocd/argo-cd
    
    Also added a comment to clarify that the Helm repo alias must match
    the name used during `helm repo add`.

--- Question-2 ArgoCD/SolutionNotes.bash
+++ Question-2 ArgoCD/SolutionNotes.bash
@@ -4,5 +4,5 @@ kubectl create namespace argocd
 # Add repo and template manifests (CRDs not installed)
 helm repo add argocd https://argoproj.github.io/argo-helm
 helm repo update
-helm template argocd argo/argo-cd --version 7.7.3 --set crds.install=false --namespace argocd > /root/argo-helm.yaml
+helm template argocd argocd/argo-cd --version 7.7.3 --set crds.install=false --namespace argocd > /root/argo-helm.yaml
 cat /root/argo-helm.yaml   # confirm output
